3faec31d5210a5910905041e91529f03eabc3fc5
Mjas360 <amosasulyman360@gmail.com>
2023-09-10 13:22:05 +0100

    set up

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import LandingPage from "./pages/landing-page/LandingPage";
+
+function App() {
+  return (
+    <div className="app">
+      <LandingPage />
+    </div>
+  );
+}
+export default App;
---
+++ src/custom-style-components/landingPageStyles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  color: #bf4f74;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+`;
